8a69d03ee0d377200f8c99bd053bfcd1b3f01f31
mrfvillanuevajr <mrfvillanuevajr@tip.edu.ph>
2023-10-23 22:39:19 +0800

    Record is now shown in the employees tab

--- controller/controllerEmployees.js
+++ controller/controllerEmployees.js
@@ -1,5 +1,9 @@
-exports.getEmployees = (req, res) => {
-    res.render('viewEmployees');
+const { PrismaClient } = require('@prisma/client');
+var prisma = new PrismaClient();
+
+exports.getEmployees = async (req, res) => {
+    const employeeInfo = await prisma.employee_Data.findMany();
+    res.render('viewEmployees', {employeeInfo: employeeInfo});
 }
 
 exports.postEmployees = (req, res) => {
--- controller/controllerEmployeesApplicants.js
+++ controller/controllerEmployeesApplicants.js
@@ -1,5 +1,9 @@
-exports.getEmployeesApplicants= (req, res) => {
-    res.render('viewEmployeesApplicants');
+const { PrismaClient } = require('@prisma/client');
+var prisma = new PrismaClient();
+
+exports.getEmployeesApplicants= async (req, res) => {
+    const employeeInfo = await prisma.employee_Data.findMany();
+    res.render('viewEmployeesApplicants', {employeeInfo: employeeInfo});
 }
 
 exports.postEmployeesApplicants = (req, res) => {
