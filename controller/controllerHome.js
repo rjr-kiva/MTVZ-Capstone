6ec758d7222caec264c9480a242d28d4f36607f9
Ronel Villanueva Jr <mrfvillanuevajr@tip.edu.ph>
2023-11-09 17:34:32 +0800

    Counts in viewHome is now working

--- controller/controllerHome.js
+++ controller/controllerHome.js
@@ -1,5 +1,14 @@
-exports.getHome = (req, res) => {
-    res.render('viewHome', {userData: req.session.userData});
+const { PrismaClient } = require('@prisma/client');
+var prisma = new PrismaClient();
+
+exports.getHome = async (req, res) => {
+
+    const employees = await prisma.employee_Data.findMany()
+    const companies = await prisma.company_Data.findMany()
+    const blacklist = await prisma.blacklisted_Data.findMany()
+    const ectag = await prisma.user_Data.findMany()
+
+    res.render('viewHome', {userData: req.session.userData, employees: employees, companies: companies, blacklist: blacklist, ectag: ectag});
 }
 
 exports.postHome = (req, res) => {
