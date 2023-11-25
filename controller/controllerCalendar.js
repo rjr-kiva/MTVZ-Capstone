9f21ba0c1db50196707e9003eddb75beacac04e3
mrfvillanuevajr <mrfvillanuevajr@tip.edu.ph>
2023-11-25 09:00:18 +0800

    Cleared all unused profileData if-else

--- controller/controllerCalendar.js
+++
@@ -1,7 +0,0 @@
-exports.getCalendar = (req, res) => {
-    res.render('viewCalendar', {userData: req.session.userData});
-}
-
-exports.postCalendar = (req, res) => {
-    res.render('viewCalendar');
-}
--- controller/controllerRecords.js
+++ controller/controllerRecords.js
@@ -3,7 +3,6 @@ var prisma = new PrismaClient();
 
 exports.getRecords = async (req, res) => {
     const employeeRecords = await prisma.employee_Data.findMany();
-    const blacklistedRecords = await prisma.blacklisted_Data.findMany();
     res.render('viewRecords', {userData: req.session.userData, employeeRecords: employeeRecords});
 }
 
